603c899cab1902fe1d9eac3d17685dad28f38fb9
Ivan-Lysenko <diparon@yandex.ru>
2024-01-26 15:45:49 +0300

    Add app.blade layout

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,2 +1,9 @@
-<h1>О блоге</h1>
-<p>Эксперементы с Laravel на Хекслете</p>
+@extends('layouts.app')
+
+@section('header')
+    <h1>О блоге</h1>
+@endsection
+
+@section('content')
+    <p>Эксперементы с Laravel на Хекслете</p>
+@endsection
